bd33ec26efc1b1df8ff8b3bb8c088c539899bb07
Xinyu <miminiao@hotmail.com>
2025-04-01 19:50:10 +0800

    get_nth ok

--- tool/converter/cad_interface.py
+++ tool/converter/cad_interface.py
@@ -5,20 +5,28 @@ from tool.converter.cad_object import CADEntity,CADBlockDef
 from lib.utils import retry,StopRetry
 
 class CADInterface:
-    @retry(delay=0.1)
-    def __init__(self, file_path:Path|str=None, version:int=23) -> None:
-        """CAD图纸读取接口.
+    """CAD图纸读取接口.
 
-        Args:
-            file_path (Paht|str, optional): dwg文件路径. Defaults to None, 读取当前活动文档的选择集内容.
-            version (int, optional): CAD版本号. Defaults to 23 (for ACAD2020).
-        """
+    Args:
+        file_path (Path|str, optional): dwg文件路径. Defaults to None (为None时读取当前活动文档的选择集).
+        version (int, optional): CAD版本号. Defaults to 23 (for ACAD2020).
+        close_doc (bool, optional): 结束时是否关闭当前文档. Defaults to False.
+        save_at_close (bool, optional): 关闭时是否保存. close_doc==True时生效. Defaults to False.
+    """
+    @retry(delay=0.1)
+    def __init__(self, file_path:Path|str=None, /,*, 
+                 version:int=23,
+                 close_at_finish:bool=False,
+                 save_at_close:bool=False) -> None:
+        self._close_at_finish=close_at_finish
+        self._save_at_close=save_at_close
         self._acad = win32com.client.Dispatch(f"AutoCAD.Application.{version}")
+        # 未传入文件路径则读取当前选择集, 传入路径则尝试打开并全选
         if file_path is None:
             self._doc = self._acad.ActiveDocument
         else:
             path=Path(file_path)
-            if not path.exists(): 
+            if not path.exists():
                 raise StopRetry(FileNotFoundError(f"'{file_path}' does not exist."))
             for doc in self._acad.Documents:
                 if Path(doc.FullName)==path.absolute():
@@ -31,42 +39,38 @@ class CADInterface:
             selection.Select(5)  # Select All
         self._selection = list(self._doc.ActiveSelectionSet)
         self.doc_name:str=self._doc.Name
-        print(f"{self.doc_name}: {len(self._selection)} entities")
+        print(f"{self.doc_name}: {len(self._selection)} entities selected.")
 
-    def close(self)->None:
-        self._doc.Close(False)
+    def __enter__(self):
+        return self
+    
+    def __exit__(self,exc_type,exc_val,exc_tb):
+        if self._close_at_finish:
+            self.close_doc(self._save_at_close)
 
-    def get_cad_objects(self,parse_block_def:bool=True)->list[CADEntity|CADBlockDef]:
+    def close_doc(self,save:bool=False)->None:
+        self._doc.Close(save)
+
+    def get_selected_objects(self,parse_block_def:bool=True)->list[CADEntity|CADBlockDef]:
         CADBlockDef.init_doc_block_table(self._doc.Blocks)
         CADBlockDef.set_parse_flag(parse_block_def)
         print("Parsing..."+(" (block def excluded)" if not parse_block_def else ""))
         res=[]
         for i,ent in enumerate(self._selection):
-            if object_name:=ent.ObjectName!="":
-                ...  # DEBUG
-            if (parsed_ent:=CADEntity.parse(ent)) is not None:
-                res.append(parsed_ent)
+            parsed_ent=CADEntity.parse(ent)
+            if parsed_ent is not None: res.append(parsed_ent)
         res.extend(CADBlockDef.parsed_blocks.values())
         print("Parsing done.")
         return res
-        # cad_geoms=flatten_blocks(cad_objects)
-        # geoms=[obj.to_geom() for obj in cad_objects if isinstance(obj,CADEntity)]
 
 if __name__=="__main__":
-    
     # from tool.converter.json_converter import JsonDumper
     # dumper=JsonDumper.default
     # dumper=JsonDumper.to_cgs
     dumper=lambda _:_.__dict__
     
-    # doc=CADInterface(r"C:\Users\Administrator\Desktop\a.dwg")
-    doc=CADInterface()
-    cad_objects=doc.get_cad_objects()
+    with CADInterface() as cad:
+        cad_objects=cad.get_selected_objects()
 
-    # a=list(filter(lambda o:o.object_name=="hatch" and o.color==251,cad_objects))
-    # b=list(filter(lambda o:o.object_name=="hatch" and o.color==5,cad_objects))
-    # cad_objects=[a,b]
-
-    # doc.close()
-    with open("./tool/converter/output/case_16.json",'w',encoding="utf8") as f:
+    with open("./tool/converter/output/case_1.json",'w',encoding="utf8") as f:
         json.dump(cad_objects,f,ensure_ascii=False,default=dumper)
